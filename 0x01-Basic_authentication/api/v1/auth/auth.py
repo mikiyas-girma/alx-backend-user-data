6c522bc5313103f9cf134c5b9a9f1f861ca23bb2
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-02 22:50:15 +0300

    check paths if they require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """" Auth module to handle API authentication """
 from flask import Flask, request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -9,8 +10,16 @@ class Auth:
     template for all authentication system in this app
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ returns paths that require auth """
-        return False
+        """ checks paths that require auth """
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        for excluded_path in excluded_paths:
+            pattern = excluded_path[:-1]
+            if re.match(pattern, path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ returns flask request object """
