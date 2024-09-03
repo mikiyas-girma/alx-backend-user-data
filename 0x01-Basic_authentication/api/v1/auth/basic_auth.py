7c5bf67f68aa53cd6ec7a99c678232c6b14c733d
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-03 12:02:23 +0300

    Basic auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Basic Auth module to handle API authentication """
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication class """
+    pass
