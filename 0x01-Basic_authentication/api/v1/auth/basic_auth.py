7a89664e53737f026c5287fa11edfd6c3ff130ea
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-03 13:24:35 +0300

    returned Base64 decoded string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth module to handle API authentication """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
 
         base64 = authorization_header.split(" ")[1]
         return base64
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ returns the decoded value of the Base64 String
+        base64_authorization_header
+        """
+        if base64_authorization_header is None or \
+            not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header, validate=True)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except Exception:
+            return None
