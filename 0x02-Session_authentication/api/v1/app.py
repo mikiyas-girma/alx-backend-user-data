05825f79070874b44f6acb1b7250a8966bf6146c
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-05 11:10:34 +0300

    added a url to list of excluded paths for auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,11 +52,14 @@ def authenticate():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
             if auth_header is None:
+                abort(401)
+            if auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
