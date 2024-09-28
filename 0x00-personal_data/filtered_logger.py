239af462cf2416dea8a832e54b3dedde3aee2113
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-28 23:10:16 +0300

    annotated the function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,10 @@
 module for filtering data
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """should use a regex to replace occurrences of certain field values
     and  returns the log message obfuscated
     """
