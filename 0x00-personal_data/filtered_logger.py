b6275b14706a7741e3d2600fa25c2fd2522fb110
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-28 23:29:55 +0300

    utilized builtin logging LogRecord object

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,13 +4,33 @@ module for filtering data
 """
 import re
 from typing import List
+import logging
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """should use a regex to replace occurrences of certain field values
-    and  returns the log message obfuscated
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """should use a regex to replace occurrences of certain
+    field values and  returns the log message obfuscated
     """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
